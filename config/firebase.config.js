0fb1ad7db153a1dd1a6c59c58cd14079f41a47ba
tanmoyM-1611 <mondal.tanmoy50@gmail.com>
2021-08-26 22:59:00 +0600

    sds

---
+++ config/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3Wi2Mynem31Q4LEZDRRul0nJfpE4OQeg",
+    authDomain: "fresh-valley-75b0d.firebaseapp.com",
+    projectId: "fresh-valley-75b0d",
+    storageBucket: "fresh-valley-75b0d.appspot.com",
+    messagingSenderId: "970440791639",
+    appId: "1:970440791639:web:7611e03fffcdd945c03512"
+  };
+
+  export default firebaseConfig;
